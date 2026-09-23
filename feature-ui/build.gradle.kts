e3f4d47fe75ae019c2cfde6695735b1763220a69
Alok Gond <166201775+AlokGond@users.noreply.github.com>
2026-09-24 02:16:09 +0530

    Fix CMPToast

--- feature-ui/build.gradle.kts
+++ feature-ui/build.gradle.kts
@@ -78,7 +78,7 @@ dependencies {
     implementation("com.mohamedrejeb.calf:calf-file-picker:0.13.0")
 
     // Toast
-    implementation("multiplatform.network.cmptoast:cmptoast:1.0.71")
+    implementation("network.chaintech:cmptoast:1.0.8")
 
     // DataStore
     implementation("androidx.datastore:datastore-preferences:1.2.1")
