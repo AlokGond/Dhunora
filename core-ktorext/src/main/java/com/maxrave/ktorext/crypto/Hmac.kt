5de8a6d3395b202efd7b7034faa6161eea18999c
Alok Gond <166201775+AlokGond@users.noreply.github.com>
2026-09-24 02:52:31 +0530

    Fix HmacUri and scraper deps

--- core-ktorext/src/main/java/com/maxrave/ktorext/crypto/Hmac.kt
+++ core-ktorext/src/main/java/com/maxrave/ktorext/crypto/Hmac.kt
@@ -59,3 +59,9 @@ class Hmac constructor(algorithm: String, secretKey: String) {
         return (currentTime - requestTime) < tokenTtl
     }
 }
+object HmacUri {
+    const val BASE_HMAC_URI = "/v1"
+    const val TRANSLATED_HMAC_URI = "/v1/translated"
+    const val VOTE_HMAC_URI = "/v1/vote"
+    const val VOTE_TRANSLATED_HMAC_URI = "/v1/translated/vote"
+}
