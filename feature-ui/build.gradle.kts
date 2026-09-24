69470211d457bbbd7c35cc6561a7a33dba5b6dc7
Alok Gond <166201775+AlokGond@users.noreply.github.com>
2026-09-24 06:09:31 +0530

    Fix UI

--- feature-ui/build.gradle.kts
+++ feature-ui/build.gradle.kts
@@ -94,6 +94,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.activity:activity-compose:1.12.2")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.1.1")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.10.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
     implementation("androidx.navigation:navigation-compose:2.9.6")
